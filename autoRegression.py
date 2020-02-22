32dfbe08ac8b389332c5be3f7ab98cca0fe94c06
Aditya Bharadwaj <adityabharadwaj@Adityas-MacBook-Pro.local>
2020-02-21 22:08:17 -0500

    auto-reg, moving_average, arma, arima, sarima methods for dummy data

---
+++ autoRegression.py
@@ -0,0 +1,49 @@
+from statsmodels.tsa.ar_model import AR
+from random import random
+from statsmodels.tsa.arima_model import ARMA
+from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+
+def auto_reg(data):
+    model = AR(data)
+    model_fit = model.fit()
+    # make prediction
+    yhat = model_fit.predict(len(data), len(data))
+    print(yhat)
+
+def moving_average(data):
+    model = ARMA(data, order=(0, 1))
+    model_fit = model.fit(disp=False)
+    # make prediction
+    yhat = model_fit.predict(len(data), len(data))
+    print(yhat)
+
+def arma_method(data):
+    model = ARMA(data, order=(2, 1))
+    model_fit = model.fit(disp=False)
+    # make prediction
+    yhat = model_fit.predict(len(data), len(data))
+    print(yhat)
+
+def arima(data):
+    model = ARIMA(data, order=(1, 1, 1))
+    model_fit = model.fit(disp=False)
+    # make prediction
+    yhat = model_fit.predict(len(data), len(data), typ='levels')
+    print(yhat)
+
+def sarima(data):
+    model = SARIMAX(data, order=(1, 1, 1), seasonal_order=(1, 1, 1, 1))
+    model_fit = model.fit(disp=False)
+    # make prediction
+    yhat = model_fit.predict(len(data), len(data))
+    print(yhat)
+
+
+data = [x + random() for x in range(1, 100)]
+#data = [random() for x in range(1, 100)]
+arima(data)
+
+
+
+
